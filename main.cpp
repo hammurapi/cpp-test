5dd750a417847fea51ef79b44ddab315e3ecf55f
Lajos Vetek <lajos.vetek@siemens.com>
2020-11-13 16:23:40 +0100

    first step (Better Code: Runtime Polymorphism - Sean Parent)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "cpp-test.h"
 
-int main( int argc, char *argv[] ) {
+using object_t = int;
+
+void draw( const object_t& x, std::ostream& out, size_t position ) {
+	out << std::string( position, ' ' ) << x << std::endl;
+}
+
+using document_t = std::vector<object_t>;
+
+void draw( const document_t& x, std::ostream& out, size_t position ) {
+	out << std::string( position, ' ' ) << "document {" << std::endl;
+	for( const auto& e : x )
+		draw( e, out, position + 2 );
+	out << std::string( position, ' ' ) << "}" << std::endl;
+}
+
+int main( int argc, char* argv[] ) {
 	auto executable_path = std::filesystem::path( argv[0] );
-	
 	std::cout << executable_path.stem().string() << " Version " << cpp_test_VERSION_MAJOR << "."
 			  << cpp_test_VERSION_MINOR << std::endl;
 
+	document_t document;
+
+	document.emplace_back( 0 );
+	document.emplace_back( 1 );
+	document.emplace_back( 2 );
+	document.emplace_back( 3 );
+
+	draw( document, std::cout, 0 );
 
 	return 0;
 }
