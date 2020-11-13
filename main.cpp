a6106b7b85e622054cc3e9d4b6d544a57ecce4cd
Lajos Vetek <lajos.vetek@siemens.com>
2020-11-13 23:22:44 +0100

    replacing unique_ptr with shared_ptr

--- main.cpp
+++ main.cpp
@@ -15,21 +15,8 @@ void draw( const T& x, std::ostream& out, size_t position ) {
 
 struct object_t {
 	template<typename T>
-	object_t( T x ) : self_( std::make_unique<model<T>>( std::move( x ) ) ) {
+	object_t( T x ) : self_( std::make_shared<model<T>>( std::move( x ) ) ) {
 	}
-
-	object_t( const object_t& x ) : self_( x.self_->copy_() ) {
-		std::cout << "copy" << std::endl;
-	}
-
-	object_t( object_t&& ) noexcept = default;
-
-	object_t& operator=( const object_t& x ) noexcept {
-		*this = object_t( x );
-		return *this;
-	}
-
-	object_t& operator=( object_t&& ) noexcept = default;
 
 	friend void draw( const object_t& x, std::ostream& out, size_t position ) {
 		x.self_->draw_( out, position );
@@ -38,17 +25,12 @@ struct object_t {
   private:
 	struct concept_t {
 		virtual ~concept_t() = default;
-		virtual std::unique_ptr<concept_t> copy_() const = 0;
 		virtual void draw_( std::ostream&, size_t ) const = 0;
 	};
 
 	template<typename T>
 	struct model final : concept_t {
 		model( T x ) : data_( std::move( x ) ) {}
-
-		std::unique_ptr<concept_t> copy_() const override {
-			return std::make_unique<model>( *this );
-		}
 
 		void draw_( std::ostream& out, size_t position ) const override {
 			draw( data_, out, position );
@@ -57,7 +39,7 @@ struct object_t {
 		T data_;
 	};
 
-	std::unique_ptr<concept_t> self_;
+	std::shared_ptr<const concept_t> self_;
 };
 
 using document_t = std::vector<object_t>;
