0ab75c96cd8bc95d3f33044f3dc81d09ee092f35
Lajos Vetek <lajos.vetek@siemens.com>
2020-11-13 15:54:49 +0100

    Adding version number

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "cpp-test.h"
+
 int main( int argc, char *argv[] ) {
+	std::cout << argv[0] << " Version " << cpp_test_VERSION_MAJOR << "."
+			  << cpp_test_VERSION_MINOR << std::endl;
+
 	std::cout << "Hello world!" << std::endl;
 
 	return 0;
