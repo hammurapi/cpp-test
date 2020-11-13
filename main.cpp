8897da165e0a586089c027f2a1c3ac1738870c03
Lajos Vetek <lajos.vetek@siemens.com>
2020-11-13 16:06:09 +0100

    spliting the arg[0]

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include <filesystem>
 #include <iostream>
 
 #include "cpp-test.h"
 
 int main( int argc, char *argv[] ) {
-	std::cout << argv[0] << " Version " << cpp_test_VERSION_MAJOR << "."
+	auto executable_path = std::filesystem::path( argv[0] );
+	
+	std::cout << executable_path.stem().string() << " Version " << cpp_test_VERSION_MAJOR << "."
 			  << cpp_test_VERSION_MINOR << std::endl;
 
 	std::cout << "Hello world!" << std::endl;
